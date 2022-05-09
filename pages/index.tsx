7d6f65050a3eedfbd8a12f561f2929649f53e507
Ashokzarmariya <ashokzarmariya47@gmail.com>
2022-05-09 10:55:35 +0530

    build home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
 import Banner from '../components/Banner'
 import Navbar from '../components/Navbar'
 import Row from '../components/Row'
